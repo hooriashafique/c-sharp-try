417daa442944de4271191fb22be79dcb4e70a2f2
hooria shafique <hr.shafique506@gmail.com>
2025-10-20 00:13:25 +0500

    Add files via upload

---
+++ getter and setter (table)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryapp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("table is created");
+
+            Table table = new Table();
+            Console.Write("please enter a number: ");
+            string strNum = Console.ReadLine();
+           
+            table.Number = int.Parse(strNum);
+
+
+            Console.Write("please enter a range: ");
+            strNum = Console.ReadLine();
+            table.Range = int.Parse(strNum);
+            //table.SetRange(Int32.Parse(strNum));
+            Console.WriteLine(" value of range:={0}", table.Range);
+            table.PrintTable();
+            Console.ReadKey();
+            
+            
+
+
+        
+
+
+        }
+    }
+
+
+}
+ 
---
+++ getter and setter (table)/Table.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryapp
+{
+    internal class Table
+    {
+        public int Number;
+        private int range;
+        public int Range
+        {
+            get
+            {
+                return range;
+            }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    range = value;
+                }
+                else { range = 0; }
+            }
+        }
+    
+
+        public Table()
+        {
+            Number = 0;
+            Range = 10; 
+        }
+        public Table(int number,int range)
+        {
+            Number = number;
+            Range = range;
+        }
+        public void PrintTable()
+        {
+            for(int i=1;i<=Range;i++)
+            {
+                Console.WriteLine("{0}x{1}={2}",Number,i,Number*i);
+                
+
+            }
+        }
+
+
+    }
+}
